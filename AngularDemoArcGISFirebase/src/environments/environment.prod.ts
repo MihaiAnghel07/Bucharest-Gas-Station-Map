1d2cbb40091ef9a904e5a5001c52d3dc7facdac5
MihaiAnghel07 <anghel.imihaigabi@gmail.com>
2022-12-20 18:03:18 +0200

    added firebase

---
+++ AngularDemoArcGISFirebase/src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  firebase: {
+    // add your Firebase config here
+    apiKey: "AIzaSyBpryQ6kgpkqpghOAwAmcSLkEyPi0rFXOg",
+    authDomain: "bucharest-gas-stations-map.firebaseapp.com",
+    databaseURL: "https://bucharest-gas-stations-map-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bucharest-gas-stations-map",
+    storageBucket: "bucharest-gas-stations-map.appspot.com",
+    messagingSenderId: "565100034158",
+    appId: "1:565100034158:web:289409cd248c3f84530e7f",
+    measurementId: "G-7JLKV8CYP8"
+  },
+  production: true
+};
+
+
+
---
+++ AngularDemoArcGISFirebase/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    // add your Firebase config here
+    apiKey: "AIzaSyBpryQ6kgpkqpghOAwAmcSLkEyPi0rFXOg",
+    authDomain: "bucharest-gas-stations-map.firebaseapp.com",
+    databaseURL: "https://bucharest-gas-stations-map-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bucharest-gas-stations-map",
+    storageBucket: "bucharest-gas-stations-map.appspot.com",
+    messagingSenderId: "565100034158",
+    appId: "1:565100034158:web:289409cd248c3f84530e7f",
+    measurementId: "G-7JLKV8CYP8"
+}, 
+  production: true
+};
+ 
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
