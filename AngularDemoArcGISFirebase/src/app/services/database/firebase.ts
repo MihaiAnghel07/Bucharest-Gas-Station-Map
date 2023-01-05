5bd7740834ccac95077a67600a3bfb0c8374c4c7
Ade51 <57103738+Ade51@users.noreply.github.com>
2023-01-05 18:13:42 +0200

    Add files via upload

--- AngularDemoArcGISFirebase/src/app/services/database/firebase.ts
+++ AngularDemoArcGISFirebase/src/app/services/database/firebase.ts
@@ -8,11 +8,22 @@ export interface ITestItem {
     lng: number
 }
 
+export interface GasStation {
+    name: string,
+    lat: number,
+    lng: number,
+    has_shop: boolean,
+    has_carwash: boolean,
+    diesel_price: number,
+    gas_price: number
+}
+
 @Injectable()
 export class FirebaseService {
 
     listFeed: Observable<any[]>;
     objFeed: Observable<any>;
+    gasStationList: Observable<any[]>;
 
     constructor(public db: AngularFireDatabase) {
 
@@ -21,6 +32,7 @@ export class FirebaseService {
     connectToDatabase() {
         this.listFeed = this.db.list('list').valueChanges();
         this.objFeed = this.db.object('obj').valueChanges();
+        this.gasStationList = this.db.list('gasStations').valueChanges();
     }
 
     getChangeFeedList() {
@@ -30,6 +42,23 @@ export class FirebaseService {
     getChangeFeedObj() {
         return this.objFeed;
     }
+
+    getGasStationsList() {
+            return this.gasStationList;
+        }
+
+    addGasStation(name: string, lat: number, lng: number, diesel_price: number, gas_price: number) {
+            let item: GasStation = {
+                name: name,
+                lat: lat,
+                lng: lng,
+                diesel_price: diesel_price,
+                gas_price : gas_price,
+                has_shop: false,
+                has_carwash: false
+            };
+            this.db.list('gasStations').push(item);
+        }
 
     addPointItem(lat: number, lng: number) {
         let item: ITestItem = {
@@ -47,5 +76,8 @@ export class FirebaseService {
             lng: lng
         };
         this.db.object('obj').set([item]);
+    }
+    removeGasStations() {
+      this.db.list("gasStations").remove();
     }
 }
