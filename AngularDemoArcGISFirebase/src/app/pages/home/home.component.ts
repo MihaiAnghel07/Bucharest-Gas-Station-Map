69605522a2143892bd80b10215748663b7775946
Ade51 <57103738+Ade51@users.noreply.github.com>
2023-01-06 16:49:13 +0200

    Add files via upload

--- AngularDemoArcGISFirebase/src/app/pages/home/home.component.ts
+++ AngularDemoArcGISFirebase/src/app/pages/home/home.component.ts
@@ -1,5 +1,14 @@
 import { Component } from "@angular/core";
 
+
+function login() {
+
+  var userEmail = document.getElementById("email_field");
+  var userPassword = document.getElementById("password_field");
+
+  window.alert(userEmail + " " + userPassword);
+}
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
